522b75164f7a2850e2f8d5dfdbafb7936dc0e16b
steve <meadsteve@gmail.com>
2020-04-07 09:36:53 +0200

    skip type checking existing elements on append

--- tilpy/__init__.py
+++ tilpy/__init__.py
@@ -1,4 +1,4 @@
-from typing import TypeVar, Generic, Tuple, Type, Iterator, List
+from typing import TypeVar, Generic, Tuple, Type, Iterator, List, Any
 
 T = TypeVar('T')
 
@@ -7,12 +7,12 @@ class Til(Generic[T]):
     _contents: Tuple[T, ...]
     _type: Type[T]
 
-    def __init__(self, *elements: T, element_type: Type[T] = None):
+    def __init__(self, *elements: T, element_type: Type[T] = None, skip_typing = False):
         self._contents = elements
         self._type = element_type or type(elements[0])
-        for element in self._contents:
-            if type(element) is not self._type:
-                raise TypeError("Elements must all be the same type")
+        if not skip_typing:
+            for element in self._contents:
+                self._assert_type(element)
 
     def as_list(self) -> List[T]:
         return list(self._contents)
@@ -23,10 +23,15 @@ class Til(Generic[T]):
         return self._contents == other._contents
 
     def append(self, new_element: T):
-        return Til(*self._contents, new_element, element_type=self._type)
+        self._assert_type(new_element)
+        return Til(*self._contents, new_element, element_type=self._type, skip_typing=True)
 
     def __iter__(self) -> Iterator[T]:
         return iter(self._contents)
 
     def __contains__(self, x: T) -> bool:
         return x in self._contents
+
+    def _assert_type(self, element: Any):
+        if type(element) is not self._type:
+            raise TypeError("Elements must all be the same type")
